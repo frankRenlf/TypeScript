e706f5955eeecf43176846e5d615185622a05f3e
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-28 19:12:01 +0800

    add

--- typescript_basic/chapter01/02_basis.js
+++ typescript_basic/chapter01/02_basis.js
@@ -2,3 +2,8 @@ var a;
 a = 10;
 var b;
 b = 'hello';
+var c = true;
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(12, 123));
--- typescript_basic/chapter01/02_basis.ts
+++ typescript_basic/chapter01/02_basis.ts
@@ -4,3 +4,9 @@ let b: String;
 b = 'hello';
 
 let c: boolean = true;
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+console.log(sum(12, 123))
