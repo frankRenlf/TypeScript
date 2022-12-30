033b8156bb98817cf135249f3b23984cfdabfcf8
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-30 20:46:43 +0800

    add

---
+++ typescript_basic/chapter01/part2/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 1;
+const _01_default_export_1 = __importDefault(require("./01_default_export"));
+console.log(_01_default_export_1.default);
--- typescript_basic/chapter01/part2/src/app.ts
+++ typescript_basic/chapter01/part2/src/app.ts
@@ -1,1 +1,3 @@
 let a = 1;
+import val  from './01_default_export'
+console.log(val)
