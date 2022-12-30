0422d73f94bc0eea670ad95ff9d9e8a0190fb83f
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-30 13:46:34 +0800

    add

--- typescript_basic/chapter01/04_types.ts
+++ typescript_basic/chapter01/04_types.ts
@@ -8,7 +8,7 @@ b = {name: 'hello'}
 let c: { name: string, [attribute: string]: any }
 c = {name: 'frank', age: 12, gender: 'male', birth: '2000-09-30'}
 
-let fn: (a: number, b: number) => number;
+let fn: (a: string, b: number) => number;
 fn = function (x, y) {
-    return x + y;
+    return x.length + y;
 }
