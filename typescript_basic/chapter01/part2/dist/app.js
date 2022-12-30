191ba5723e918f1bfc55baea633cc297bee3bf15
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-30 21:02:15 +0800

    add

--- typescript_basic/chapter01/part2/dist/app.js
+++ typescript_basic/chapter01/part2/dist/app.js
@@ -1,8 +1,49 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-let a = 1;
-const _01_default_export_1 = __importDefault(require("./01_default_export"));
-console.log(_01_default_export_1.default);
+System.register("01_default_export", [], function (exports_1, context_1) {
+    "use strict";
+    var a, b;
+    var __moduleName = context_1 && context_1.id;
+    function sum(x, y) {
+        return x + y;
+    }
+    return {
+        setters: [],
+        execute: function () {
+            a = 10;
+            b = 20;
+            exports_1("default", {
+                a, sum
+            });
+        }
+    };
+});
+System.register("02_import", ["01_default_export"], function (exports_2, context_2) {
+    "use strict";
+    var _01_default_export_1;
+    var __moduleName = context_2 && context_2.id;
+    return {
+        setters: [
+            function (_01_default_export_1_1) {
+                _01_default_export_1 = _01_default_export_1_1;
+            }
+        ],
+        execute: function () {
+            console.log(_01_default_export_1.default);
+        }
+    };
+});
+System.register("app", ["01_default_export"], function (exports_3, context_3) {
+    "use strict";
+    var a, _01_default_export_2;
+    var __moduleName = context_3 && context_3.id;
+    return {
+        setters: [
+            function (_01_default_export_2_1) {
+                _01_default_export_2 = _01_default_export_2_1;
+            }
+        ],
+        execute: function () {
+            a = 1;
+            console.log(_01_default_export_2.default);
+        }
+    };
+});
