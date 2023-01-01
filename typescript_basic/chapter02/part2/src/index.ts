48497aca411b9f34f8cca9669402e52a3e9dc7bd
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-01 19:20:13 +0800

    add

--- typescript_basic/chapter02/part2/src/index.ts
+++ typescript_basic/chapter02/part2/src/index.ts
@@ -1,1 +1,10 @@
 import './style/index.less'
+
+class Food {
+    // define position
+    element: HTMLElement;
+
+    constructor() {
+        this.element = document.getElementById('food')!;
+    }
+}
