b6a5c0f9fd4ee3c17d2848098abc828416ce2b7d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-01 19:29:39 +0800

    add

--- typescript_basic/chapter02/part2/src/index.ts
+++ typescript_basic/chapter02/part2/src/index.ts
@@ -1,10 +1,28 @@
 import './style/index.less'
 
 class Food {
-    // define position
+    // define el
     element: HTMLElement;
 
     constructor() {
         this.element = document.getElementById('food')!;
     }
+
+    // define position
+    get X() {
+        return this.element.offsetLeft;
+    }
+
+    get Y() {
+        return this.element.offsetTop;
+    }
+
+    // modify position
+    change() {
+        this.element.style.left = String(Math.round(Math.random() * 29) * 10);
+        this.element.style.top =
+    }
 }
+
+const food = new Food();
+console.log(food.X, food.Y)
