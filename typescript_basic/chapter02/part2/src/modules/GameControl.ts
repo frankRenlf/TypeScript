60e0add19fa0026acba056163a84423d561c1376
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-01 22:47:57 +0800

    add

--- typescript_basic/chapter02/part2/src/modules/GameControl.ts
+++ typescript_basic/chapter02/part2/src/modules/GameControl.ts
@@ -1,16 +1,27 @@
 import Snake from "./Snake";
 import ScorePanel from "./ScorePanel";
 import Food from "./Food";
+import snake from "./Snake";
 
-class GameControl{
-    snake:Snake;
-    scorePanel:ScorePanel;
-    food:Food;
+class GameControl {
+    snake: Snake;
+    scorePanel: ScorePanel;
+    food: Food;
 
     constructor() {
         this.snake = new Snake();
         this.scorePanel = new ScorePanel();
         this.food = new Food();
+        this.init();
+    }
+
+    init() {
+        document.addEventListener('keydown', this.keyDownHandler);
+    }
+
+    keyDownHandler(event: KeyboardEvent) {
+        console.log(this.snake.X, this.snake.Y);
     }
 }
+
 export default GameControl
