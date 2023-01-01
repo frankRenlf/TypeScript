db5384981d0803712e241aa7093c8e1efd74a181
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-01 15:55:15 +0800

    add

---
+++ typescript_basic/chapter02/part2/webpack.config.js
@@ -0,0 +1,72 @@
+// import a package
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+// webpack configuration
+module.exports = {
+    mode: "production",
+    // input file entry
+    entry: "./src/index.ts",
+    // pack the output file
+    output: {
+        // pack dir
+        path: path.resolve(__dirname, 'dist'),
+        // output file name
+        filename: "bundle.js",
+        /*
+        environment: {
+            arrowFunction: false
+        }
+        */
+    },
+    // modules in package
+    module: {
+        // added rules
+        rules: [
+            {
+                // rules for test
+                test: /\.ts$/,
+                use: [
+                    // config babel
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            // set Predefine
+                            presets: [
+                                [
+                                    // appoint env
+                                    "@babel/preset-env",
+                                    // config info
+                                    {
+                                        // Browser to be compatible
+                                        targets: {
+                                            "chrome": "88"
+                                        },
+                                        // set core-js edition
+                                        "corejs": "3",
+                                        // the use of core-js
+                                        "useBuiltIns": "usage",
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    'ts-loader'
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    // config the webpack plugin
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            // title: "custom title"
+            template: "./src/index.html"
+        }),
+    ],
+    // set the import modules
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+};
