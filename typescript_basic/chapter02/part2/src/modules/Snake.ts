00833db5f29eade1b7750f954585eb807b53a607
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-01 20:27:31 +0800

    add

--- typescript_basic/chapter02/part2/src/modules/Snake.ts
+++ typescript_basic/chapter02/part2/src/modules/Snake.ts
@@ -1,11 +1,35 @@
 class Snake {
 
     head: HTMLElement;
-    section:HTMLCollection;
+    sectionList: HTMLCollection;
+    element: HTMLElement;
 
     constructor(head: HTMLElement) {
+        this.element = document.getElementById('snake')!;
         this.head = document.querySelector('#snake>div')!;
+        this.sectionList = this.element.getElementsByTagName('div');
     }
+
+    get X() {
+        return this.head.offsetLeft;
+    }
+
+    get Y() {
+        return this.head.offsetTop;
+    }
+
+    set X(value: number) {
+        this.head.style.left = value + 'px';
+    }
+
+    set Y(value: number) {
+        this.head.style.top = value + 'px';
+    }
+
+    addSection() {
+        this.element.insertAdjacentHTML("beforeend", "<div></div>");
+    }
+
 }
 
 export default Snake;
