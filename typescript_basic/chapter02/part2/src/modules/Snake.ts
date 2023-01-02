93cdecc75e7ff24e1010bc9a540f899d829427e0
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-02 14:25:25 +0800

    add

--- typescript_basic/chapter02/part2/src/modules/Snake.ts
+++ typescript_basic/chapter02/part2/src/modules/Snake.ts
@@ -21,7 +21,7 @@ class Snake {
     set X(value: number) {
         if (this.X === value) return;
         if (value < 0 || value > 290) {
-            throw new Error('Hit the wall')
+            throw new Error('Hit the wall!')
         }
         this.head.style.left = value + 'px';
     }
@@ -29,7 +29,7 @@ class Snake {
     set Y(value: number) {
         if (this.Y === value) return;
         if (value < 0 || value > 290) {
-            throw new Error('Hit the wall')
+            throw new Error('Hit the wall!')
         }
         this.head.style.top = value + 'px';
     }
